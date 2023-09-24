91bca04b109fa3ff8cdcb55cb5cdd0fc0ccbf90b
chakra2017 <bhattacharjee.chakra17@gmail.com>
2023-09-24 10:31:06 +0530

    test updates

--- src/components/RestaurentCard.js
+++ src/components/RestaurentCard.js
@@ -4,7 +4,7 @@ import UserContext from "../utils/UserContext";
 const RestaurantCard = (props) => {
   const { resData } = props;
   const { loggedInUser } = useContext(UserContext);
-
+  //console.log(resData);
   const {
     cloudinaryImageId,
     name,
